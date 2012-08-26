21fbb4de9c5df7e6dabb5b29f9b90d7c64b1a2aa
Assil Ksiksi <cyph0nik@gmail.com>
2012-08-26 15:13:23 +0400

    Date is now a filter + minor changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sqlite3, requests, json
 from flask import Flask, request, url_for, render_template
-from database import *
 from parse import *
-from time import time, asctime
+from time import asctime
      
 # App config
 DATABASE = '' # Not required yet
@@ -15,9 +14,12 @@ app.config.from_object(__name__)
 # Steam API key: must be defined
 API_KEY = ''
 
-# Current date
-now = asctime()
-date = now[4:10] + now[19:]
+# Return current date
+@app.template_filter('date')
+def date():
+    now = asctime()
+    date = now[4:10] + now[19:]
+    return date
 
 # Main page
 @app.route('/')
@@ -29,18 +31,18 @@ def index():
 def search():
     return render_template('search.html', date=date)
 
-# Allows result retrieval using custom URL
+# Result retrieval using custom URL
 @app.route('/bp/<profile>')
-def backpack(profile):
+def bp(profile):
     return redirect(url_for('result', s=profile, t='profile'))
 
-# Show results
+# Get search params and pass them to backpack page
 @app.route('/result', methods=['GET'])
 def result():
     # Start timer
     start = time()
-    
-    # Get form parameters
+
+    # Get params
     parameter = request.args.get('s')
     category = request.args.get('t')
 
@@ -48,20 +50,20 @@ def result():
     steamid = find_steamid(parameter, category)
 
     # Get responses if steamid is valid
-    responses = {'item': None, 'player': None}
+    responses = {'items': None, 'player': None}
     if steamid:
         responses['player'] = get_player_response(steamid, API_KEY)
-        responses['item'] = get_item_response(steamid, API_KEY)
+        responses['items'] = get_item_response(steamid, API_KEY)
 
     # Get parsed responses
     responses['player'] = parse_player_response(responses['player'])
-    responses['item'] = parse_item_response(responses['item'])
+    responses['items'] = parse_item_response(responses['items'])
 
     # Add bp_slots to player dict and remove it from item dict
-    if responses['player'] and responses['item'] > 0:
-        responses['player']['Backpack Slots'] = '{0}/{1}'.format(len(responses['item'][0]), responses['item'].pop(1))
+    if responses['player'] and responses['items'] > 0:
+        responses['player']['Backpack Slots'] = '{0}/{1}'.format(len(responses['items'][0]), responses['items'].pop(1))
 
-    return render_template('result.html', player=responses['player'], items=responses['item'], time=time()-start, date=date)
+    return render_template('result.html', player=responses['player'], items=responses['items'], time=time()-start)
 
 # About page
 @app.route('/about')
